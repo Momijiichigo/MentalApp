8ceeabc6acbbfe59efc28a672a6f2928c1505cbf
Momijiichigo <ichigomomiji436@gmail.com>
2019-09-21 23:05:50 -0700

    aaa

--- pwaTest/sw.js
+++ pwaTest/sw.js
@@ -10,6 +10,7 @@ self.addEventListener('install', function(event) {
     caches.open(CACHE_NAME)
     .then((cache)=> {      
         console.log('[ServiceWorker] Pre-caching offline page');
+        alert(FILES_TO_CACHE);
         return cache.addAll(FILES_TO_CACHE);    
     })
   );
